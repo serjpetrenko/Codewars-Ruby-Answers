9a00ede598b1faf94dab19e6be577b0b23f772bd
serjpetrenko <b.g.lupus@gmail.com>
2017-01-04 01:07:26 +0200

    Changed name of directory Added new files Changed info inside readme.md Added new files 4 January

---
+++ Answers 8kyu/Ones_and_Zeros.rb
@@ -0,0 +1,6 @@
+def binary_array_to_number(arr)
+  # your code here
+
+  arr.join("").to_i(2)
+
+end
---
+++ Answers 8kyu/Switch_it_up.rb
@@ -0,0 +1,4 @@
+def switch_it_up(number)
+  # Write your code here...
+  ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+end
