c34982e06e21f5dc0c42450106e391f675efd807
serjpetrenko <b.g.lupus@gmail.com>
2017-01-04 23:07:39 +0200

    Added new files 4 January part 2

---
+++ Answers 8kyu/I_love_you.rb
@@ -0,0 +1,5 @@
+def how_much_i_love_you(nb_petals)
+ # code here
+  i = (nb_petals - 1) % 6
+  ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][i]
+end
---
+++ Answers 8kyu/Regular_ball.rb
@@ -0,0 +1,9 @@
+class Ball
+  # Your code goes here
+  def initialize(ball_type ="regular")
+     @ball_type = ball_type
+   end
+  def ball_type
+    @ball_type
+  end
+end
